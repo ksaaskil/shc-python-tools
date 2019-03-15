ca600ce0e4dbec33057b09de6afa25864ca44d5e
Kimmo Sääskilahti <ksaaskil@gmail.com>
2019-03-15 12:04:24 +0200

    Clean up SHCPostProc.

--- sdhc/SHCPostProc.py
+++ sdhc/SHCPostProc.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-# Kimmo Sääskilahti, 2015
-from __future__ import division
+from __future__ import division, print_function
 import numpy as np
 
 __all__ = ["SHCPostProc"]
@@ -84,28 +82,28 @@ class SHCPostProc(object):
 
         for key, value in args.items():
             if not hasattr(self, key):
-                raise ValueError, "Invalid argument " + key + " to PostProc!"
-            print "Using the value " + key + "=" + str(value) + "."
+                raise ValueError("Invalid argument " + key + " to PostProc!")
+            print("Using the value " + key + "=" + str(value) + ".")
             setattr(self, key, value)
 
         import os
         if self.reCalcVels or not os.path.isfile(self.compactVelocityFile):  # Check if the velocity file exists
             # Check that the LAMMPS Dump file exists
             if self.LAMMPSDumpFile is None or not os.path.isfile(self.LAMMPSDumpFile):
-                raise ValueError, "You must give the LAMMPS velocity dump file as an argument to create the file " + self.compactVelocityFile + "!"
+                raise ValueError("You must give the LAMMPS velocity dump file as an argument to create the file " + self.compactVelocityFile + "!")
             # print self.compactVelocityFile + " does not exist, creating by reading from file " + self.LAMMPSDumpFile
             # Run the C++ script
             self._compactVels(self.LAMMPSDumpFile, self.compactVelocityFile)
 
         else:
-            print self.compactVelocityFile + " exists, using the file for post-processing."
+            print(self.compactVelocityFile + " exists, using the file for post-processing.")
 
         # Check the force constant file
         if self.reCalcFC or not os.path.isfile(
                 self.KijFilePrefix + '.Kij.npy'):  # Check if the force constant file exists
-            print "Creating file " + self.KijFilePrefix + "."
+            print("Creating file " + self.KijFilePrefix + ".")
             if self.LAMMPSRestartFile is None:
-                raise ValueError, "You must give the LAMMPSRestartFile as an argument so that the file " + self.KijFilePrefix + ".Kij.npy can be created!"
+                raise ValueError("You must give the LAMMPSRestartFile as an argument so that the file " + self.KijFilePrefix + ".Kij.npy can be created!")
             self._calcFC(self.KijFilePrefix, self.LAMMPSRestartFile)
         else:  # Load the force constants from file
             self._loadFC(self.KijFilePrefix)
@@ -120,37 +118,36 @@ class SHCPostProc(object):
         from fcCalc import fcCalc
         with fcCalc(fileprefix, restartfile) as fc:
             fc.preparelammps(pair_style='sw', pair_coeff='* * Si_vbwm.sw Si', w_interface=3.0)
-            # hstep=0.001
             fc.fcCalc(self.hstep)
             fc.writeToFile()
             self.Kij = fc.Kij  # Reference
-            print "Size of the Kij file is (3*%d)x(3*%d)." % (np.size(self.Kij, 0) / 3, np.size(self.Kij, 1) / 3)
+            print("Size of the Kij file is (3*%d)x(3*%d)." % (np.size(self.Kij, 0) / 3, np.size(self.Kij, 1) / 3))
             self.ids_L = fc.ids_L  # Reference
             self.ids_R = fc.ids_R  # Reference
             self.NL = len(self.ids_L)
-            print "len(ids_L)=%d" % (self.NL)
+            print("len(ids_L)=%d" % self.NL)
             self.NR = len(self.ids_R)
-            print "len(ids_R)=%d" % (self.NR)
+            print("len(ids_R)=%d" % self.NR)
 
     def _loadFC(self, KijFilePrefix):
-        print "Loading the force constants from " + KijFilePrefix + '.Kij.npy'
+        print("Loading the force constants from " + KijFilePrefix + '.Kij.npy')
         self.Kij = np.load(KijFilePrefix + '.Kij.npy')
-        print "Size of the Kij file is (3*%d)x(3*%d)." % (np.size(self.Kij, 0) / 3, np.size(self.Kij, 1) / 3)
-        print "Loading left interfacial atom indices from " + KijFilePrefix + '.ids_L.npy'
+        print("Size of the Kij file is (3*%d)x(3*%d)." % (np.size(self.Kij, 0) / 3, np.size(self.Kij, 1) / 3))
+        print("Loading left interfacial atom indices from " + KijFilePrefix + '.ids_L.npy')
         self.ids_L = np.load(KijFilePrefix + '.ids_L.npy')
-        print "Loading right interfacial atom indices from " + KijFilePrefix + '.ids_R.npy'
+        print("Loading right interfacial atom indices from " + KijFilePrefix + '.ids_R.npy')
         self.ids_R = np.load(KijFilePrefix + '.ids_R.npy')
         self.NL = len(self.ids_L)
-        print "len(ids_L)=%d" % (self.NL)
+        print("len(ids_L)=%d" % self.NL)
         self.NR = len(self.ids_R)
-        print "len(ids_R)=%d" % (self.NR)
+        print("len(ids_R)=%d" % self.NR)
         if (np.size(self.Kij, 0) / 3 != self.NL) or (np.size(self.Kij, 1) / 3 != self.NR):
-            raise ValueError, "Sizes in Kij and ids_L/R do not match!"
+            raise ValueError("Sizes in Kij and ids_L/R do not match!")
 
     def _compactVels(self, fileVels, finalFileVels):
         from subprocess import call
         command = ["compactify_vels", fileVels, finalFileVels]
-        print "Running " + " ".join(command)
+        print("Running " + " ".join(command))
         call(command)
 
     def _smoothen(self, df, func, widthWin):
@@ -168,27 +165,25 @@ class SHCPostProc(object):
         :return: None
         """
 
-        print "Reading the compact velocity file " + self.compactVelocityFile + "."
+        print("Reading the compact velocity file " + self.compactVelocityFile + ".")
         f = open(self.compactVelocityFile, 'r')
         s = f.readline().split()
         NAtoms = int(s[1])
-        # print NAtoms, self.NL, self.NR
         if NAtoms != self.NL + self.NR:
-            raise ValueError, 'Mismatch in the numbers of atoms in the read velocity file and the used force constant file!'
+            raise ValueError('Mismatch in the numbers of atoms in the read velocity file and the used force constant file!')
 
         s = f.readline()
-        print s
+        print(s)
         s = s.split()
         self.sampleTimestep = int(s[1]) * self.dt_md
 
         s = f.readline()  # Atom ids:
-        print s
+        print(s)
         # Read the atom ids
         indArray = np.fromfile(f, dtype=int, count=NAtoms, sep=" ")
-        # print indArray
 
         s = f.readline()  # ------
-        print s
+        print(s)
 
         # Total number of degrees of freedom
         NDOF = 3 * (self.NL + self.NR)
@@ -204,12 +199,12 @@ class SHCPostProc(object):
 
         for k in np.arange(self.NChunks):  # Start the iteration over chunks
             #        for k in range(0,2): # Start the iteration over chunks
-            print "Chunk %d/%d." % (k + 1, self.NChunks)
+            print("Chunk %d/%d." % (k + 1, self.NChunks))
             # Read a chunk of velocitites
             velArray = np.fromfile(f, dtype=np.dtype('f8'), count=self.chunkSize * NDOF, sep=" ")
             # Prepare for exit if the read size does not match the chunk size
             if np.size(velArray) == 0:
-                print "Finished the file, exiting."
+                print("Finished the file, exiting.")
                 self.NChunks = k - 1
                 break
             elif np.size(velArray) != self.chunkSize * NDOF:
@@ -222,7 +217,7 @@ class SHCPostProc(object):
                     exitFlag = True
                     self.oms_fft = np.fft.rfftfreq(self.chunkSize, d=self.sampleTimestep) * 2 * np.pi
                     Nfreqs = np.size(self.oms_fft)
-                    print "Changing chunk size to " + str(int(np.size(velArray) / NDOF)) + "!"
+                    print("Changing chunk size to " + str(int(np.size(velArray) / NDOF)) + "!")
 
             # Reshape the array so that each row corresponds to different degree of freedom (e.g. particle 1, direction x etc.)
             velArray = np.reshape(velArray, (NDOF, self.chunkSize), order='F')
@@ -284,14 +279,13 @@ class SHCPostProc(object):
                 break
             else:  # This should never be reached
                 assert False, "SHCPostProc should not reach here (exitFlag=True and k>0)."
-                break
 
         # Calculate the error estimate at each frequency from the between-chunk variances
         if self.NChunks > 1:
-            print "Calculating error estimates."
+            print("Calculating error estimates.")
             samplevar = (self.NChunks / (self.NChunks - 1.0)) * (self.SHC_smooth2 - self.SHC_smooth ** 2)
             self.SHC_error = np.sqrt(samplevar) / np.sqrt(self.NChunks)
         else:
             self.SHC_error = None
 
-        print "Finished post-processing."
+        print("Finished post-processing.")
