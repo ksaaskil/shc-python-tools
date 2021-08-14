32bec8683969c4e2a7d9e92dd5aea41de5579e75
Kimmo Sääskilahti <ksaaskil@gmail.com>
2021-08-14 17:22:04 +0300

    Add logging config.

--- sdhc/config.py
+++ sdhc/config.py
@@ -1,5 +1,12 @@
 import logging
 
-logging.basicConfig()
 
+def setup_logger():
+    logging.basicConfig(
+        level=logging.INFO, format="%(name)s [%(levelname)s]: %(message)s"
+    )
+    # logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+
+setup_logger()
 logger = logging.getLogger("sdhc")
