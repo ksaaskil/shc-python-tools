794dda829bdd023c8d63bbcf63b5e8a821ee8286
Kimmo Sääskilahti <ksaaskil@gmail.com>
2021-12-04 17:11:17 +0200

    Add tests that run fast.

--- tests/test_shdc.py
+++ tests/test_shdc.py
@@ -4,20 +4,20 @@ from sdhc.SHCPostProc import SHCPostProc
 RESOURCES_PATH = Path("tests") / "resources"
 
 
-def compute_sdhc(folder: Path, restart_file: Path):
+def compute_sdhc_e2e(folder: Path, restart_file: Path):
 
-    compact_velocities_file = folder.joinpath("vels.dat.compact")
-    atomic_velocities_file = folder.joinpath("simu.vels.dat")
+    output_compact_velocities_file = folder.joinpath("small.simu.vels.dat.compact")
+    atomic_velocities_file = folder.joinpath("small.simu.vels.dat")
     frequency_window_width = 0.5e12
 
     backup_prefix = folder.joinpath("backup")
-    force_constant_file_prefix = folder.joinpath("force_constants")
+    output_force_constant_file_prefix = folder.joinpath("force_constants")
     unit_scaling_factor = 1.602e-19 / 1e-20 * 1e4
     md_timestep = 2.5e-15
 
     postprocessor = SHCPostProc(
-        compactVelocityFile=str(compact_velocities_file),
-        KijFilePrefix=str(force_constant_file_prefix),
+        compactVelocityFile=str(output_compact_velocities_file),
+        KijFilePrefix=str(output_force_constant_file_prefix),
         dt_md=md_timestep,
         scaleFactor=unit_scaling_factor,
         LAMMPSDumpFile=str(atomic_velocities_file),
@@ -26,8 +26,8 @@ def compute_sdhc(folder: Path, restart_file: Path):
         chunkSize=50000,
         backupPrefix=str(backup_prefix),
         LAMMPSRestartFile=str(restart_file),
-        reCalcVels=True,
-        reCalcFC=True,
+        reCalcVels=False,
+        reCalcFC=False,
     )
 
     postprocessor.postProcess()
@@ -39,6 +39,6 @@ def test_compute_sdhc():
     folder = RESOURCES_PATH
     restart_file = RESOURCES_PATH.joinpath("quenched.restart")
 
-    postprocessor = compute_sdhc(folder=folder, restart_file=restart_file)
+    postprocessor = compute_sdhc_e2e(folder=folder, restart_file=restart_file)
     assert postprocessor.oms_fft is not None
     assert postprocessor.SHC_smooth is not None
