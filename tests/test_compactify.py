eac5bc3e840c81671854b441cf2971534115d54e
Kimmo Sääskilahti <ksaaskil@gmail.com>
2021-12-04 16:40:36 +0200

    Add small test data and a unit test for compactify script.

---
+++ tests/test_compactify.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import subprocess
+import tempfile
+
+RESOURCES_PATH = Path("tests") / "resources"
+
+VELS_DAT_PATH = RESOURCES_PATH / "small.simu.vels.dat"
+
+
+def test_compactify():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmpd = Path(tmpdir)
+        out_path = tmpd / "vels.dat.compact"
+        res = subprocess.run(
+            ["./scripts/compactify_vels", str(VELS_DAT_PATH), str(out_path)]
+        )
+        assert res.returncode == 0
+
+        assert out_path.is_file(), f"Expected to exist: {out_path}"
+
+        content = out_path.read_text()
+        expected_content = (RESOURCES_PATH / "small.simu.vels.dat.compact").read_text()
+
+        assert content == expected_content
